973577cb84626f83ea12b15792b7a7c9c1f2cc44
Luceta <57941164+Luceta@users.noreply.github.com>
2023-08-24 23:10:31 +0900

    chore: list , map, set

--- lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -31,3 +31,17 @@ final bankMap = {
   "shinhan1": bankAccountShinhan1,
   "shinhan2": bankAccountShinhan2,
 };
+
+//Set = bankSet.contains() 를 통해서 아이템 유무 파악을 위해 주로 사용
+// 아래처럼 8개를 넣어도 Print시 5개만 나옴.
+// bankSet.toList() // bankAcount.toSet()
+final bankSet = {
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao
+};
